a84bf1e19eef988b3f284d2a18e3dbb243cc8ac9
mohamad zareai <mohamadzareai@gmail.co>
2021-10-11 11:50:18 +0330

    update create countext

--- src/comtext/CounterOne.js
+++ src/comtext/CounterOne.js
@@ -4,12 +4,18 @@ import { useContext } from "react";
 import { CounterContext, CounterContextDispatcher } from "./CounterProvider";
 function CounterOne() {
   // useContext`
-  const count = useContext(CounterContext);//state
-  const setCount = useContext(CounterContextDispatcher);//set state
+  const count = useContext(CounterContext); //state
+  const setCount = useContext(CounterContextDispatcher); //set state
   return (
     <div>
-      <h1>count is :{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Add One</button>
+      <h1>
+        count is :<span className={count >= 0 ? "green" : "red"}>{count}</span>
+      </h1>
+      {/* <button onClick={() => setCount(count + 1)}>Add One</button> */}
+      {/* ican use prev */}
+      <button onClick={() => setCount((perv) => perv + 1)}>Add One</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount((perv) => perv - 1)}>minus One</button>
     </div>
   );
 }
--- src/comtext/CounterProvider.js
+++ src/comtext/CounterProvider.js
@@ -17,3 +17,4 @@ const CounterProvider = ({children} ) => {
 };
 
 export default CounterProvider;
+
