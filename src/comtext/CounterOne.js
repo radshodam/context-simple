c22e9b2e95433a780ee6e894f32bcdde2039c68c
mohamad zareai <mohamadzareai@gmail.co>
2021-10-11 12:16:24 +0330

    seprate actions

--- src/comtext/CounterOne.js
+++ src/comtext/CounterOne.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { useContext } from "react";
 // imp conterContext
-import { CounterContext, CounterContextDispatcher } from "./CounterProvider";
+import { CounterContext, useCountActions } from "./CounterProvider";
 function CounterOne() {
   // useContext`
   const count = useContext(CounterContext); //state
-  const setCount = useContext(CounterContextDispatcher); //set state
+  //actions
+  const actions = useCountActions(); //set state
+
+  //obj Distructcher
+  const { addOne, reset, minusOne } = useCountActions(); //set state
+
+  console.log(actions);
   return (
     <div>
       <h1>
         count is :<span className={count >= 0 ? "green" : "red"}>{count}</span>
       </h1>
-      {/* <button onClick={() => setCount(count + 1)}>Add One</button> */}
-      {/* ican use prev */}
-      <button onClick={() => setCount((perv) => perv + 1)}>Add One</button>
-      <button onClick={() => setCount(0)}>reset</button>
-      <button onClick={() => setCount((perv) => perv - 1)}>minus One</button>
+      \<button onClick={addOne}>Add One</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={minusOne}>minus One</button>
     </div>
   );
 }
 
 export default CounterOne;
+//actions
--- src/comtext/CounterProvider.js
+++ src/comtext/CounterProvider.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 //create context
 export const CounterContext = React.createContext(); //state
 //Dispatcher
 export const CounterContextDispatcher = React.createContext(); //setstate
 //countProvider
-const CounterProvider = ({children} ) => {
+const CounterProvider = ({ children }) => {
   const [count, setCount] = useState(0);
 
   return (
@@ -17,4 +17,22 @@ const CounterProvider = ({children} ) => {
 };
 
 export default CounterProvider;
+//actions
+export const useCount = () => {
+  useContext(CounterContext);
+};
+//actions And set
+export const useCountActions = () => {
+  const setCount = useContext(CounterContextDispatcher);
 
+  const addOne = () => {
+    setCount((perv) => perv + 1);
+  };
+  const minusOne = () => {
+    setCount((perv) => perv - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+  return { addOne, minusOne, reset };
+};
